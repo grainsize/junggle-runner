6dc9d0b046cb8e55b49e885aabeb3f74db435345
grainsize <81246964+grainsize@users.noreply.github.com>
2026-01-10 15:56:34 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,21 @@
 const CACHE_NAME = "jungle-runner-v1";
+const ASSETS = [
+  "/jungle-runner/",
+  "/jungle-runner/index.html",
+  "/jungle-runner/manifest.json",
+  "/jungle-runner/icon-192.png",
+  "/jungle-runner/icon-512.png"
+];
 
 self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null)))
-    )
-  );
-  self.clients.claim();
-});
-
 self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
