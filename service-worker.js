917464df5b7d9ad20fb51e43511ef9bf9082d9b4
grainsize <81246964+grainsize@users.noreply.github.com>
2026-01-10 11:19:20 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,16 @@
-self.addEventListener("install", e => {
+const CACHE_NAME = "jungle-runner-v1";
+
+self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
 });
+
+self.addEventListener("fetch", event => {
